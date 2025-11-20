e151196d7b1d5826c4e0eea76430f172ad5883c4
simonherz2 <simonhockey.91@gmail.com>
2025-11-20 09:40:27 -0800

    Clean repo: remove node_modules & build artifacts from tracking

--- dist/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = errorHandler;
-function errorHandler(err, req, res, next) {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
-}
